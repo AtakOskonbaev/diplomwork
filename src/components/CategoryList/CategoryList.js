746200d0cc4648082777c8dbc6ed16ee990c895a
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-07 14:50:08 +0600

    added navigation hover animations and added category list

---
+++ src/components/CategoryList/CategoryList.js
@@ -0,0 +1,9 @@
+export default function CategoryList() {
+  const CategoryList = [
+    {id:1, name:"constructors"},
+    {id:2, name:"baby toys"},
+    {id:3, name:"girl toys"},
+    {id:4, name:"soft toys"},
+    {id:5, name:"nastolka"},
+  ]
+}
