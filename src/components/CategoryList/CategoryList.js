39c2099282d332a40126af071f9574bcd0af6fd2
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-07 14:58:37 +0600

    created new output with categories and links

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,9 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 export default function CategoryList() {
-  const CategoryList = [
-    {id:1, name:"constructors"},
-    {id:2, name:"baby toys"},
-    {id:3, name:"girl toys"},
-    {id:4, name:"soft toys"},
-    {id:5, name:"nastolka"},
-  ]
+  const categories = [
+    { id: 1, name: "constructors", slug: "constructors" },
+    { id: 2, name: "baby toys", slug: "baby-toys" },
+    { id: 3, name: "girl toys", slug: "girl-toys" },
+    { id: 4, name: "soft toys", slug: "soft-toys" },
+    { id: 5, name: "nastolka", slug: "nastolka" },
+  ];
+
+  const output = categories.map((category) => (
+    <li key={category.id}>
+      <NavLink to={"/categories" + category.slug}>
+        {category.name}
+      </NavLink>
+    </li>
+  ))
+
+  return (
+    <div className="CategoryList">
+      <h3>Categories</h3>
+      <ul>{output}</ul>
+    </div>
+  )
 }
