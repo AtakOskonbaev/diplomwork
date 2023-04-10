4e25f80f00bd58ffda9a7b4c8fd441e140bfbd7c
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-10 14:45:33 +0600

    changed colors of the site

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1jz7Bb_l8hrF4WR-0yP1pZkJKYLDbHx0",
+  authDomain: "diplomwork-8d546.firebaseapp.com",
+  projectId: "diplomwork-8d546",
+  storageBucket: "diplomwork-8d546.appspot.com",
+  messagingSenderId: "1021219130334",
+  appId: "1:1021219130334:web:b9f14cd0fb321220ce2b4f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const categories = collection.()
