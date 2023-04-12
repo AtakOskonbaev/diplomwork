c0d867450aaf04940249c109c7703da352ab53c5
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-12 15:18:11 +0600

    added new code that brings data from firebase

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,25 +1,21 @@
 import { useMatch } from "react-router-dom";
 import NotFound from "./NotFound";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 
 export default function Category() {
-  const {params} = useMatch("/categories/:slug");
+  const { params } = useMatch("/categories/:slug");
 
-  const categories = [
-    { id: 1, name: "constructors", slug: "constructors" },
-    { id: 2, name: "baby toys", slug: "baby-toys" },
-    { id: 3, name: "girl toys", slug: "girl-toys" },
-    { id: 4, name: "soft toys", slug: "soft-toys" },
-    { id: 5, name: "nastolka", slug: "nastolka" },
-  ];
+  const { categories } = useContext(AppContext);
 
   const category = categories.find(
     (category) => category.slug === params.slug
   );
 
-    if(!category){
-      return <NotFound />;
-    }
+  if (!category) {
+    return <NotFound />;
+  }
 
   return (
     <div className="Category">
