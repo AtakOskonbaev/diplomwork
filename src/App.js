66c1a00a39b7ebc1ba60bea291d3bbe29950d0d0
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-14 13:47:52 +0600

    remade code that provides data via firebase

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import Delivery from "./pages/Delivery";
 import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import { createContext, useEffect, useState } from "react";
-import { categoryCollection } from "./firebase";
+import { categoryCollection, productsCollection } from "./firebase";
 import { getDocs } from "firebase/firestore/lite";
 
 export const AppContext = createContext({
   categories: [],
+  products: [],
 });
 
 function App() {
   const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     getDocs(categoryCollection)
@@ -29,9 +31,21 @@ function App() {
       })
   }, []);
 
+  useEffect(() => {
+    getDocs(productsCollection)
+      .then(({ docs }) => {
+        setProducts(
+          docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+          }))
+        )
+      })
+  }, []);
+
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories }} >
+      <AppContext.Provider value={{ categories, products }} >
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
