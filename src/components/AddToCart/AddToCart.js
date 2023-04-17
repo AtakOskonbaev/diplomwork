0f24cdf98f8b4275d92b73f5201e44b6fff3eab4
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-17 14:58:37 +0600

    added code that adds products to cart

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -3,15 +3,20 @@ import "./AddToCart.css";
 import { AppContext } from "../../App";
 
 export function AddToCart({ product }) {
-  const [cart, setCart] = useContext(AppContext);
+  const { cart, setCart } = useContext(AppContext);
 
   function onAddToCartClick() {
-    setCart([]);
+    const qty = cart[product.id] ? cart[product.id] + 1 : 1;
+    setCart({
+      ...cart,
+      [product.id]: qty,
+    });
   }
 
   return (
     <div className="AddToCart">
       <button onClick={onAddToCartClick}>Add to cart</button>
+      {cart[product.id] ? cart[product.id] : 0}
     </div>
-  )
+  );
 }
