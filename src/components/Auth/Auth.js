09b5892f403e489edb74e1d2c5018994bbd57eaf
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-19 15:46:07 +0600

    new function of deleting sign in button when youve already signed in

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,13 +1,15 @@
-import { signIn } from "../../firebase";
-import "./Auth.css";
+import { useContext } from "react";
+import { signIn, signOff } from "../../firebase";
+import { AppContext } from "../../App";
 
 export default function Auth() {
+  const { user } = useContext(AppContext);
 
   return (
-    <div className="Auth" >
-      <button onClick={signIn}>
-        Sing In
-      </button>
+    <div className="Auth">
+      {!user ? <button onClick={signIn}>Sign in</button> : null}
+      {user ? <span>{user.displayName}</span> : null}
+      {user ? <button onClick={signOff}>Sign out</button> : null}
     </div>
-  )
+  );
 }
