df667bf112e195623fc0ffcfac7f28948c3bd3c9
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-23 20:20:07 +0600

    deleted cart link and auth button to move them to nav

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,6 @@
 import Nav from "../Nav/Nav";
 import "./Layout.css";
 import CategoryList from "../CategoryList/CategoryList";
-import CartLink from "../CartLink/CartLink";
-import Auth from "../Auth/Auth";
 
 export default function Layout(props) {
   return (
@@ -10,8 +8,6 @@ export default function Layout(props) {
 
       <header>
         <Nav />
-        <CartLink />
-        <Auth />
       </header>
       <aside>
         <CategoryList />
