a6b96eef0a952ca0dca242e5a225e1ece42057c2
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-24 15:46:30 +0600

    covered username  wiith link to orders

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,6 +2,8 @@ import "./Auth.css";
 import { useContext } from "react";
 import { signIn, signOff } from "../../firebase";
 import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+import Orders from "../../pages/Orders";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
@@ -9,7 +11,7 @@ export default function Auth() {
   return (
     <div className="Auth">
       {!user ? <button onClick={signIn}>Sign in</button> : null}
-      {user ? <span>{user.displayName}</span> : null}
+      {user ? <span><Link to="/orders">{user.displayName}</Link></span> : null}
       {user ? <button onClick={signOff}>Sign out</button> : null}
     </div>
   );
