a911dc8ca109a7f748821cc52cab40e828fd925c
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-24 14:45:14 +0600

    fixed some bugs and added search section

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -6,7 +6,10 @@ export default function HeaderSection() {
   return (
     <div className="HeaderSection">
       <Logo />
-      <input type="text" placeholder="Catalog search" />
+      <div className="search">
+        <input type="text" placeholder="Catalog search" />
+        <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
       <div>
         <p>Daily from 10am to 10pm</p>
         <p>+996 <span>123 345 567</span></p>
