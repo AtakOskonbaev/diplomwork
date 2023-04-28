a6cd4d6f9e289737392cc2e29931862d1d5a69ac
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-28 14:42:39 +0600

    added logic to addcategory that access to add new category to admin

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,19 +1,41 @@
 import { useState } from "react";
 import "./AddCategory.css";
+import { addDoc } from "firebase/firestore/lite";
+import { categoryCollection } from "../../firebase";
 
 export default function AddCategory() {
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("")
 
   function onChangeCategory(event) {
     setCategory(event.target.value)
   }
-
+  
   function onAddCategory() {
-    
+    const name = category.trim();
+
+    if (name.length < 5) {
+      alert("Please provide a category name with minimum length of 5 characters.")
+
+    }
+    addDoc(categoryCollection, {
+      name: category.trim(),
+      slug: category.trim()
+        .replace(' ', '-')
+        .toLocaleLowerCase(),
+    }).then(() => {
+      setCategory("");
+    });
   }
 
-  <div className="AddCategory">
-    <input type="text" value={category} placeholder="Category name" onChange={onChangeCategory} />
-    <button onClick={onAddCategory}>+</button>
-  </div>
+  return (
+    <div className="AddCategory">
+      <input
+        size="15"
+        type="text"
+        placeholder="Category name"
+        value={category}
+        on onChange={onChangeCategory}
+      />
+      <button onClick={onAddCategory}>+</button>
+    </div>)
 }
