a42a5e3e4fbc33db52ae99e5ccf1c66648bef24f
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-28 15:36:50 +0600

    created new files and added check if there are products

---
+++ src/components/DeleteCategory/DeleteCategory.js
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import "./DeleteCategory.css";
+import { AppContext } from "../../App";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase";
+
+export default function DeleteCategory({category}) {
+  const { user, products } = useContext(AppContext);
+
+  if(!user || !user.isAdmin) {
+    return null;
+  }
+
+  function onDeleteClick() {
+    const count = Object.values(products).filter(product => product.category === category.id).length;
+
+    if (count > 0) {
+      alert("This category has existing products. Please delete them before deleting a category.");
+
+      return;
+    }
+
+    deleteDoc(doc(db, "categories", category.id));
+  }
+
+  return (
+      <button className="DeleteCatrgory" onClick={onDeleteClick}>-</button>
+  )
+}
