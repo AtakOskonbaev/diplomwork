f47fa09be1372b33ad8943a7b0e1e1a4d8735909
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-04-28 15:13:18 +0600

    added admin auth that accesses to admins to add new categories

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,22 +1,30 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./AddCategory.css";
-import { addDoc } from "firebase/firestore/lite";
+import { addDoc } from "firebase/firestore";
 import { categoryCollection } from "../../firebase";
+import { AppContext } from "../../App";
 
 export default function AddCategory() {
   const [category, setCategory] = useState("")
+  const { user } = useContext(AppContext);
+
+  if(!user || !user.isAdmin ) {
+    return null;
+  }
 
   function onChangeCategory(event) {
     setCategory(event.target.value)
   }
-  
+
   function onAddCategory() {
     const name = category.trim();
 
     if (name.length < 5) {
       alert("Please provide a category name with minimum length of 5 characters.")
 
+      return;
     }
+
     addDoc(categoryCollection, {
       name: category.trim(),
       slug: category.trim()
