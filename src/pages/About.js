1a43779809ff1e459ffb8f04fc6990c01b52f4c3
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-01 14:43:53 +0600

    added description to the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,13 @@ export default function About() {
   return (
     <div className="About">
       <h1>About</h1>
+      <p>Magic toys - a chain of toy stores with more than 20 years of history!
+        Magic toys stores sell children's toys for any age and wallet, from rattles to strollers and cribs.</p>
+      <p>Cars, dolls, designers, bicycles, swings, rollers, skates, balls, scooters and much more.</p>
+      <p>The wide range includes products from world famous manufacturers such as Fishes Price, Tomy, Disney, Mamalove, Carter's, Blue-Box Toys, PlaySkeol, Mastela, Little Tikes, Saturn, Hot Wheels, Mattel, etc.</p>
+      <p>Adult buyers can also find interesting things here: ceramic products products for decoration of the interior of the house and office, stuffed toys.</p>
+      <p>In the New Year's Eve season, a wide range of Christmas trees, Christmas tree and New Year dyeing, tinsel, dry snow, products for Christmas and Christmas decor are presented.</p>
+      <p>Products from Magic Toys are also attractive due to the availability of prices. Also, we have a permanent promotion «Look for yellow tags»</p>
     </div>
   )
 }
