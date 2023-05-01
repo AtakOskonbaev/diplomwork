fc73a31ef376ce929913bda517abe400d480d421
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-01 14:06:47 +0600

    imported add product button to product list

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,6 +3,7 @@ import "./ProductList.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
 import { AddToCart } from "../AddToCart/AddToCart";
+import AddProduct from "../AddProduct/AddProduct";
 
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
@@ -26,6 +27,8 @@ export default function ProductList({ category }) {
   return (
     <div className="ProductList">
       {output}
+      
+      <AddProduct />
     </div>
   )
 }
