c0e69a82a0560c8c07fb91f32946ed717653c39d
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-01 15:08:29 +0600

    added icons and desription into contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,6 +2,12 @@ export default function Contacts() {
   return (
     <div className="Contacts">
       <h1>Contacts</h1>
+      <h2>Our contacts:</h2>
+      <p><i className="fa fa fa-phone" /> +996 123 123 123</p>
+      <p><i className="fa fa-map-marker" /> GUM, 3rd floorBishkek Park, 2nd floor</p>
+      <p><i className="fa fa-map-marker" /> Tash-Rabat</p>
+      <p><i className="fa fa-map-marker" /> Asia Mall</p>
+      <p><i class="fa-brands fa-youtube" /> <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">YouTube</a></p>
     </div>
   )
 }
