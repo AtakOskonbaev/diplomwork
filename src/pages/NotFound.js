778a984ccf29bc03eecda97c31cf3d51852678ac
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-03 14:54:33 +0600

    edited text from "404" to  "this page does not exist"

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ import rickroll from "../assets/rickroll-qr-code.png";
 export default function NotFound() {
   return (
     <div className="NotFound">
-      <h1>404</h1>
+      <h1>This page does not exist</h1>
       <img src={rickroll} alt="rickroll-qr-code"/>
     </div>
   );
