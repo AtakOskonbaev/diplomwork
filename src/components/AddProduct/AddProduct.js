f6ee5ada8f6bf52a8c5f0a38af45a0a98969083c
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-10 14:23:22 +0600

    added 3 functions for creating new product

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,9 +1,24 @@
 import "./AddProduct.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../App";
 
 export default function AddProduct() {
   const { user } = useContext(AppContext);
+  const [ name, setName ] = useState("");
+  const [ price, setPrice ] = useState(0);
+  const [ picture, setPicture ] = useState(null);
+
+  function onChangeName(event) {
+    setName(event.target.value);
+  }
+  function onChangePrice(event) {
+    setPrice(event.target.value);
+  }
+  function onChangePicture(event) {
+    const file = event.target.files[0];
+    setPicture(file);
+  }
+
 
   if (!user || !user.isAdmin) {
     return null;
@@ -24,7 +39,7 @@ export default function AddProduct() {
           Name: <input type="text" name="name" required />
         </label>
         <label>
-          Price: <input type="number" name="price" min={0} step="any" required /> 
+          Price: <input type="number" name="price" min={0} step="any" required />
         </label>
         <label>
           Picture: <input type="file" name="picture" required />
