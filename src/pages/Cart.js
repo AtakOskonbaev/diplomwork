79e4e094a82b444534e8d70b0a8db5c10b34bf85
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-11 19:08:02 +0600

    added new const to connect cart and order form

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import CartList from "../components/CartList/CartList";
 import OrderForm from "../components/OrderForm/OrderForm";
 
 export function Cart() {
+  const [modalActive, setModalActive] = useState(true);
+
   return (
     <div className="Cart" >
       <h1>Cart</h1>
       <CartList />
-      <OrderForm />
+      <OrderForm active={modalActive} setActive={setModalActive} />
     </div>
   )
 }
