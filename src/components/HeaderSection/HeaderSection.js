e0222430a3c2789f4e7d1eba7357019e60d9e651
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-13 21:54:16 +0600

    imported and used random product button in header section

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -1,15 +1,14 @@
 import "./HeaderSection.css";
 import Logo from "../Logo/Logo";
 import CartLink from "../CartLink/CartLink";
+import RandomProductRedirect from "../RandomProductRedirect/RandomProductRedirect";
 
 export default function HeaderSection() {
+
   return (
     <div className="HeaderSection">
       <Logo />
-      <div className="search">
-        <input type="text" placeholder="Catalog search" className="searcher" />
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </div>
+        <RandomProductRedirect/>
       <div className="info">
         <p>Daily from 10am to 10pm</p>
         <p>+996 <span className="pink-text">123 345 567</span></p>
