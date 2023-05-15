ca8b5e6ccaf9e51ca556d995d8c563297adc4073
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-15 15:45:17 +0600

    imported bestsellers into homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-
+import HitProducts from "../components/HitProducts/HitProduct";
 
 export default function Home() {
   return (
     <div className="Home">
       <h1>Home</h1>
+      <HitProducts/>
     </div>
   )
 }
