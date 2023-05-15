6577204e1f6a57664f4507617646d38f8336c3dc
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-15 18:28:54 +0600

    added random product redirect when click the button

--- src/components/RandomProductRedirect/RandomProductRedirect.js
+++ src/components/RandomProductRedirect/RandomProductRedirect.js
@@ -1,12 +1,21 @@
+import { useContext } from "react";
 import "./RandomProductRedirect.css";
+import { AppContext } from "../../App";
 
 export default function RandomProductRedirect() {
+  const { products } = useContext(AppContext);
 
-  return (
-    <div className="RandomProductRedirect">
-      <button>
-        Перейти к случайному продукту
-      </button>
-    </div>
-  )
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * products.length);
+    const randomProduct = products[randomIndex];
+    window.location.href = "/products/" + randomProduct.slug;
+  }
+
+    return (
+      <div className="RandomProductRedirect">
+        <button onClick={handleClick}>
+          Перейти к случайному продукту
+        </button>
+      </div>
+    )
 }
