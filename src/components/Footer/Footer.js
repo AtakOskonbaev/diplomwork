ee122eb957756227601139bb2b64369b0da954e2
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-17 15:30:20 +0600

    created footer and added styles for it

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,20 +4,26 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <ul className="footerItem">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contacts">Contacts</NavLink>
-        </li>
-        <li>
-          <NavLink to="/delivery">Delivery</NavLink>
-        </li>
-      </ul>
+      <div className="footer-links">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contacts">Contacts</NavLink>
+          </li>
+          <li>
+            <NavLink to="/delivery">Delivery</NavLink>
+          </li>
+        </ul>
+        <i class="fa-brands fa-instagram" />
+      </div>
+      <div className="info">
+        ToyHub - online shop © 2023
+      </div>
     </div>
   )
 }
