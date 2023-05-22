38a89c8862043fbc3ef133981bd18f429c7c4e6c
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-22 15:16:38 +0600

    fixed a problem with order button in cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import CartList from "../components/CartList/CartList";
 import OrderForm from "../components/OrderForm/OrderForm";
+import { AppContext } from "../App";
 
 export function Cart() {
   const [modalActive, setModalActive] = useState(false);
+  const { cart } = useContext(AppContext);
+  if (Object.keys(cart).length === 0) {
+    return (
+    <div className="Cart" >
+      <h1>Cart</h1>
+      <CartList />
+      <OrderForm active={modalActive} setActive={setModalActive} />
+    </div>
+    )
+  }
 
   return (
     <div className="Cart" >
