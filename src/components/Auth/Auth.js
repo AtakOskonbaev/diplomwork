a30935edc584e07870839139e247eeb3a112b3aa
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-05-22 14:46:17 +0600

    added mobile version of nav and new icon

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -11,7 +11,8 @@ export default function Auth() {
     <div className="Auth">
       {!user ? <button onClick={LogIn}>Sign in</button> : null}
       {user ? <span><Link to="/orders">{user.displayName}</Link></span> : null}
-      {user ? <button onClick={LogOut}>Sign out</button> : null}
+      {user ? <button className="icon"><Link to="/orders"><i class="fa-solid fa-user" /></Link></button> : null}
+      {user ? <button className="blueBtn" onClick={LogOut}>Sign out</button> : null}
     </div>
   );
 }
